abfaed971845b9865e0ed67e1aa404d707b6ea66
JS-404-000 <hondarr@naver.com>
2023-04-27 10:38:48 +0900

    ui설계 및 버튼 블락드

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.implicitintent;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -8,10 +9,13 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,14 +30,34 @@ public class MainActivity extends AppCompatActivity {
                 (ContextCompat.checkSelfPermission(getApplicationContext(), PERMISSIONS[1])
                         != PackageManager.PERMISSION_GRANTED))
             ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
+        /* 위에 코드 이해불가 시 통으로 외우기 */
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode== 1){
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Button btnCall = findViewById(R.id.btnCall);
+                btnCall.setEnabled(false);
+            }
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Button btnCamera = findViewById(R.id.btnCamera);
+                btnCamera.setEnabled(false);
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
+        if ((requestCode == 1) && (resultCode == RESULT_OK)) {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView ivCamera = findViewById(R.id.iVCamera);
+            ivCamera.setImageBitmap(bitmap);
+        }
     }
 
     public void onClick(View view) {
